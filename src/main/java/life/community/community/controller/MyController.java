e3300f6f238c4bcfa2905137a54810b11b62c4fb
zhangzhe <821560326@qq.com>
2019-12-24 21:17:03 +0800

    fist

---
+++ src/main/java/life/community/community/controller/MyController.java
@@ -0,0 +1,20 @@
+package life.community.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MyController {
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name = "name",
+            required=false, defaultValue="World") String name,
+                           @RequestParam(name = "pwd",
+                                   required=false, defaultValue="World") String pwd
+            ,Model model) {
+        model.addAttribute("name", name);
+        model.addAttribute("pwd",pwd);
+        return "greeting";
+    }
+}
