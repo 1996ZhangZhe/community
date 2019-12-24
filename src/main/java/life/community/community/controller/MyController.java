39eaa9a1cf67864eab246332662ce7e0c850deeb
zhangzhe <821560326@qq.com>
2019-12-24 23:24:32 +0800

    frist

--- src/main/java/life/community/community/controller/MyController.java
+++ src/main/java/life/community/community/controller/MyController.java
@@ -17,4 +17,5 @@ public class MyController {
         model.addAttribute("pwd",pwd);
         return "greeting";
     }
+
 }
---
+++ src/main/java/life/community/community/provider/GithubProvider.java
@@ -0,0 +1,8 @@
+package life.community.community.provider;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class GithubProvider {
+
+}
